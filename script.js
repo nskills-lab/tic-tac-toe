a3de39f2a7f4f010a0d17055ceaae85822ef5e6c
Nina Rey <reynina.w@gmail.com>
2022-07-01 15:23:31 -0600

    Add a way to draw pixelated X

--- script.js
+++ script.js
@@ -15,4 +15,37 @@ function loadPixels() {
   }
 }
 
+function colorX(x1, y1, x2, y2) {
+  let xStart = x1 + 3;
+  let yStart = y1 + 3;
+  let xEnd = x2 - 3;
+  let yEnd = y2 - 3;
+
+  for (let yCor = yStart, xCor = xStart; yCor <= yEnd; yCor++, xCor++) {
+    colorOneXGrid(xCor, yCor, "red");
+  }
+
+  for (let yCor = yStart, xCor = xEnd; yCor <= yEnd; yCor++, xCor--) {
+    colorOneXGrid(xCor, yCor, "red");
+  }
+}
+
+function colorOneXGrid(xCor, yCor, color) {
+  let pixel = document.querySelector(
+    `div[data-x-coordinate="${xCor}"][data-y-coordinate="${yCor}"]`
+  );
+  pixel.setAttribute("style", `background-color:${color}`);
+}
+
 loadPixels();
+colorX(1, 1, 17, 17);
+colorX(17, 17, 33, 33);
+
+// Draw a pixaletated circle
+// Listen to the click and based on the player decide whether to draw x or circle
+// mark the cell whether it is x or circle
+// Keep track of player moves
+// generate AI's move
+// decide who is the winner or if it is a draw
+// show the winner (outlay)
+// button restarts the game
